ed350e8f3d08d86abea95392d12389d8ebedf045
dan4onarkoman40 <dd93578373@edu.mon.bg>
2024-10-14 10:35:54 +0300

    First commit

---
+++ mengova/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using mengova.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mengova.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet <Category> mengova { get; set; }
+    }
+}
---
+++ mengova/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mengova.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
